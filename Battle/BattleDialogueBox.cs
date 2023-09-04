4c415497562cfa7029228249c2ea8b3954e4a5a5
Akaash Banerjee <plunkels@gmail.com>
2023-09-04 23:40:40 +0800

    update Battle System

---
+++ Battle/BattleDialogueBox.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleDialogueBox : MonoBehaviour
+{
+    [SerializeField] int lettersPerSecond;
+    [SerializeField] Text dialogueText;
+    [SerializeField] GameObject actionSelector;
+    [SerializeField] GameObject moveSelector;
+    [SerializeField] GameObject moveDetails;
+
+    [SerializeField] List<Text> actionTexts;
+    [SerializeField] List<Text> moveTexts;
+
+    [SerializeField] Text ppText;
+    [SerializeField] Text typeText;
+
+    public void setDialogue(string dialogue)
+    {
+        dialogueText.text = dialogue;
+
+    }
+
+    public IEnumerator TypeDialogue (string dialogue)
+    {
+        dialogueText.text = "";
+        foreach (var letter in dialogue.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(1f/lettersPerSecond);
+        }
+    }
+
+    public void EnableDialogueText(bool enabled)
+    {
+        dialogueText.enabled = enabled;
+    }
+
+    public void EnableActionSelector(bool enabled)
+    {
+        actionSelector.SetActive(enabled);
+    }
+
+    public void EnableMoveSelector(bool enabled)
+    {
+        moveSelector.SetActive(enabled);
+        moveDetails.SetActive(enabled);
+    }
+}
--- Battle/BattleSystem.cs
+++ Battle/BattleSystem.cs
@@ -2,17 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum BattleState { Start, PlayerAction, PlayerMove, EnemyMove, Busy }
+
 public class BattleSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+   [SerializeField] BattleUnit playerUnit;
+   [SerializeField] BattleUnit enemyUnit;
+   [SerializeField] BattleHUD playerHUD;
+   [SerializeField] BattleHUD enemyHUD;
+   [SerializeField] BattleDialogueBox dialogueBox;
+
+   BattleState state;
+
+   public void Start()
+   {
+        StartCoroutine(SetupBattle());
+   }
+
+    public IEnumerator SetupBattle()
     {
-        
+        playerUnit.Setup();
+        playerHUD.SetData(playerUnit.Pokemon);
+        enemyUnit.Setup();
+        enemyHUD.SetData(enemyUnit.Pokemon);
+
+        yield return dialogueBox.TypeDialogue($"A wild {enemyUnit.Pokemon.Base.Name} appeared!");
+        yield return new WaitForSeconds(1f);
+
+        PlayerAction();
     }
 
-    // Update is called once per frame
-    void Update()
+    void PlayerAction()
     {
+        state = BattleState.PlayerAction;
+        StartCoroutine(dialogueBox.TypeDialogue($"What will {playerUnit.Pokemon.Base.Name} do?"));
+        dialogueBox.EnableActionSelector(true);
         
     }
 }
